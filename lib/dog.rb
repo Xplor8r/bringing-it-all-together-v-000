8777bde519921a98c6046173531b047317ea75dc
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-29 02:07:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,5 +68,13 @@ def save
     self.new(id, name, breed)
   end
   
-  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
+  end
 end
