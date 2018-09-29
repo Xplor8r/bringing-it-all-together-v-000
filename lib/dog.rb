9bf6be6b482bf4a43581d66b9055482ebd04e5b3
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-29 02:02:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,11 +45,13 @@ def save
   
   def self.find_by_name(id)
     sql = <<-SQL
-      SELECT * FROM students
+      SELECT * FROM dogs
       WHERE id = ?
       LIMIT 1
     SQL
     
     DB[:conn].execute(sql, id).map { |row| self.new_from_db(row) }.first
   end
+  
+  
 end
